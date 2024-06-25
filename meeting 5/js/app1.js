74a1dc1a7b869363080e2bd7745ac11a1d7313ab
TiffaniLie <103040305+TiffaniLie@users.noreply.github.com>
2024-06-25 14:18:37 +0700

    Add files via upload

---
+++ meeting 5/js/app1.js
@@ -0,0 +1,13 @@
+function openCute(evt, cuteName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cuteName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
